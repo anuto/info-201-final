4d71d4dc022e553d6cc76b6f8de73455cb36b52b
Alissa Adornato <Alissa Adornato>
2017-05-28 15:48:09 -0700

    added scripts folder and map file

---
+++ scripts/map.R
@@ -0,0 +1,43 @@
+# map :D
+
+# temporary data finding
+# assault.data <- read.csv("./data/title.ix.cases.csv", stringsAsFactors = FALSE)
+
+# I want: number of cases (count), 
+#         percent of cases to population (pop.percent), 
+#         ratio of close to unclosed (unclosed.ratio),
+#         longitude (long),
+#         latitude (lat)
+#         :D
+assault.map <- function(assault.data) {
+
+  g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showland = TRUE,
+    landcolor = toRGB("black"),
+    subunitcolor = toRGB("white"),
+    countrywidth = 0.5,
+    subunitwidth = 0.5
+  )
+  
+  plot_geo(assault.data, lat = ~lat, lon = ~long) %>%
+    add_markers(
+      text = ~paste(date, 
+                    city, 
+                    paste("total cases: ", count),
+                    paste("% unclosed cases: ", unclosed.ratio * 100), 
+                    paste("cases per 100 students: ", pop.percent), 
+              sep = "<br />"),
+      color = ~unclosed.ratio, 
+      colors = c("#ffffff", "#ff0000"),
+      symbol = I("circle"), 
+      size = ~pop.percent, 
+      hoverinfo = "text"
+    ) %>%
+    colorbar(title = "Cases per 100 Students") %>%
+    layout(
+      title = 'Shootings over 2016', geo = g
+    )
+
+}
