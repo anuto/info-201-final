246a80c6264ca739bd12ace522e10792b3b5198c
anuto <ayp2@uw.edu>
2017-05-31 20:14:30 -0700

    css stuff

--- ui.R
+++ ui.R
@@ -25,35 +25,35 @@ shinyUI(fluidPage(
                                               Additionally, we create a time line to present the mishandled sexual assault cases. We also 
                                               provide our insightful findings and suggestion for your reference.'),                    
              fluidRow(
-               column(2, thumbnail_label(image = NULL, label = 'Explore by location',
+               column(4, thumbnail_label(image = NULL, label = 'Explore by location',
                                          content = 'See the distribution of reported sexual assaults across 
                                          college campuses in the US over the last decade. Compare and contrast
                                          colleges by population, sector, and gender ratio.',
                                          button_link = 'http://getbootstrap.com/', button_label = 'Explore map')
                ),
-               column(2, thumbnail_label(image = "", label = 'Explore mishandled cases',
+               column(3, thumbnail_label(image = "", label = 'Explore mishandled cases',
                                          content = 'In 2015 Department of Education investigated sexual assault
                                          cases involving student athletes. They found 400 cases to have been
                                          mishandled.',
                                          button_link = 'http://getbootstrap.com/', button_label = 'Explore cases')
                ),
-               column(2, thumbnail_label(image = "", label = 'Explore our solution',
+               column(4, thumbnail_label(image = "", label = 'Explore our solution',
                                          content = 'Whether you\'re a student or university employee, here are
                                          some ways you can do your part to help protect students',
                                          button_link = 'http://getbootstrap.com/', button_label = 'Get involved')
                ),
-               column(2, thumbnail_label(image = 'C:/Users/Althea/Desktop/info201/info-201-final/pics/map.PNG', label = 'Explore our findings',
+               column(4, thumbnail_label(image = 'C:/Users/Althea/Desktop/info201/info-201-final/pics/map.PNG', label = 'Explore our findings',
                                          content = 'Whether you\'re a student or university employee, here are
                                          some ways you can do your part to help protect students',
                                          button_link = 'http://getbootstrap.com/', button_label = 'Get involved')
                
                ),
-               column(2, thumbnail_label(image = "", label = 'Understanding our Data',
+               column(3, thumbnail_label(image = "", label = 'Understanding our Data',
                                          content = 'Whether you\'re a student or university employee, here are
                                          some ways you can do your part to help protect students',
                                          button_link = 'http://getbootstrap.com/', button_label = 'See more')
                ),
-               column(2, thumbnail_label(image = "", label = 'Meet the Team',
+               column(3, thumbnail_label(image = "", label = 'Meet the Team',
                                          content = 'Whether you\'re a student or university employee, here are
                                          some ways you can do your part to help protect students',
                                          button_link = 'http://getbootstrap.com/', button_label = 'See more')
